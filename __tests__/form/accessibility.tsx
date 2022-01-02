a576bccc2f10dd7a4f42a00899b8b91b91640669
Jesse Wright <jesse.wright@anu.edu.au>
2022-01-02 19:46:30 +1100

    chore: re-enable disabled tests

--- __tests__/form/accessibility.tsx
+++ __tests__/form/accessibility.tsx
@@ -7,32 +7,32 @@ import { unsupportedShapes } from '../data';
 
 expect.extend(toHaveNoViolations);
 
-// describe('Testing the accesibility of generated forms', () => {
-//   it('Should have no violations when rendering any form', async () => {
-//     const NodeShapes = await InferencedProxiedNodeShapes;
-//     for (const shape of NodeShapes) {
-//       if (!unsupportedShapes.includes(`${shape}`)) {
-//         // console.log(`${shape}`);
-//         const { container } = render(<MockForm shape={shape} />);
-//         /* eslint-disable no-await-in-loop */
-//         const results = await axe(container);
-//         expect(results).toHaveNoViolations();
-//       }
-//     }
-//   }, 30000);
-// });
+describe('Testing the accesibility of generated forms', () => {
+  it('Should have no violations when rendering any form', async () => {
+    const NodeShapes = await InferencedProxiedNodeShapes;
+    for (const shape of NodeShapes) {
+      if (!unsupportedShapes.includes(`${shape}`)) {
+        // console.log(`${shape}`);
+        const { container } = render(<MockForm shape={shape} />);
+        /* eslint-disable no-await-in-loop */
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+      }
+    }
+  }, 30000);
+});
 
-// describe('Testing the accesibility of generated with LDfields plugin', () => {
-//   it('Should have no violations when rendering any form', async () => {
-//     const NodeShapes = await InferencedProxiedNodeShapes;
-//     for (const shape of NodeShapes) {
-//       if (!unsupportedShapes.includes(`${shape}`)) {
-//         // console.log(`${shape}`);
-//         const { container } = render(<MockFormLDfields shape={shape} />);
-//         /* eslint-disable no-await-in-loop */
-//         const results = await axe(container);
-//         expect(results).toHaveNoViolations();
-//       }
-//     }
-//   }, 30000);
-// });
+describe('Testing the accesibility of generated with LDfields plugin', () => {
+  it('Should have no violations when rendering any form', async () => {
+    const NodeShapes = await InferencedProxiedNodeShapes;
+    for (const shape of NodeShapes) {
+      if (!unsupportedShapes.includes(`${shape}`)) {
+        // console.log(`${shape}`);
+        const { container } = render(<MockFormLDfields shape={shape} />);
+        /* eslint-disable no-await-in-loop */
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+      }
+    }
+  }, 30000);
+});
--- __tests__/form/form-change.tsx
+++ __tests__/form/form-change.tsx
@@ -6,25 +6,25 @@ import { MockForm } from '../mocks';
 
 const PersonShape = 'http://datashapes.org/sh/tests/core/complex/personexample.test#PersonShape';
 
-// describe('Testing consistency rendering of forms', () => {
-//   it('Correctly renders a basic form', async () => {
-//     const tree = renderer.create(
-//       <MockForm
-//         shape={(await ProxiedNodeShapesMapPromise)[PersonShape]}
-//       />,
-//     ).toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
-//   it('Correctly renders all supported forms in the test suite', async () => {
-//     for (const shape of await InferencedProxiedNodeShapes) {
-//       if (!unsupportedShapes.includes(`${shape}`)) {
-//         const tree = renderer.create(
-//           <MockForm
-//             shape={shape}
-//           />,
-//         ).toJSON();
-//         expect(tree).toMatchSnapshot();
-//       }
-//     }
-//   }, 30000);
-// });
+describe('Testing consistency rendering of forms', () => {
+  it('Correctly renders a basic form', async () => {
+    const tree = renderer.create(
+      <MockForm
+        shape={(await ProxiedNodeShapesMapPromise)[PersonShape]}
+      />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('Correctly renders all supported forms in the test suite', async () => {
+    for (const shape of await InferencedProxiedNodeShapes) {
+      if (!unsupportedShapes.includes(`${shape}`)) {
+        const tree = renderer.create(
+          <MockForm
+            shape={shape}
+          />,
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+      }
+    }
+  }, 30000);
+});
